6e70c02e035d776254d617ed1476493938e69f37
Alberto Vilar <albertovilar1@gmail.com>
2025-03-26 20:37:58 -0300

    first commit

---
+++ src/main/java/com/vilardev/capril_vilar/CaprilVilarApplication.java
@@ -0,0 +1,13 @@
+package com.vilardev.capril_vilar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaprilVilarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaprilVilarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vilardev/capril_vilar/CaprilVilarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vilardev.capril_vilar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaprilVilarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
